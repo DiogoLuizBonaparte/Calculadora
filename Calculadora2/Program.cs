120f1f5ea4a56ba5a4b365178c684d1b0d327eff
Diogo-Monteiro322 <diogo.monteiro@totvs.com.br>
2023-12-03 15:50:01 -0300

    Primeiro commit

---
+++ Calculadora2/Program.cs
@@ -0,0 +1,149 @@
+﻿namespace Calculadora2
+{
+
+    internal class Program
+    {
+        enum EOpcoes { Somar = 1, Subtrair, Multiplicar, Dividir, Potencia, Raiz, Sair }
+
+        public static void Main(string[] args)
+        {
+
+            bool saiu = false;
+            while (!saiu)
+            {
+
+                Opcoes();
+                EOpcoes EOpcaoEscolhida = (EOpcoes)int.Parse(Console.ReadLine());
+
+                switch (EOpcaoEscolhida)
+                {
+                    case EOpcoes.Somar:
+                        Somar();
+                        break;
+                    case EOpcoes.Subtrair:
+                        Subtrair();
+                        break;
+                    case EOpcoes.Multiplicar:
+                        Multiplicar();
+                        break;
+                    case EOpcoes.Dividir:
+                        Dividir();
+                        break;
+                        break;
+                    case EOpcoes.Potencia:
+                        Potencia();
+                        break;
+                        break;
+                    case EOpcoes.Raiz:
+                        Raiz();
+                        break;
+                    case EOpcoes.Sair:
+                        Sair();
+                        saiu = true;
+                        break;
+                    default:
+                        TratamentoErro();
+                        break;
+                }
+                Console.ReadLine();
+                Console.Clear();
+
+            }
+        }
+
+
+    static void Opcoes()
+        {
+            Console.WriteLine("Escolha uma opção");
+            Console.WriteLine("\"1 - Somar\n2 - Subtrair\n3 - Multiplicar\n4 - Divdir\n5 - Potencia\n6 - Raiz\n7 - Sair\"");
+        }
+
+        static void TratamentoErro()
+        {
+            Console.WriteLine("Opção invalida.");
+             
+        }    
+
+        static void Somar()
+        {
+            Console.WriteLine("Você escolheu somar \n");
+            Console.WriteLine("Digite o primeiro número");
+            int soma1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            int soma2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da soma é: {soma1 + soma2}");
+           
+        }
+        static void Subtrair()
+        {
+            Console.WriteLine("Vocês escolheu subtrair \n");
+            Console.WriteLine("Digite o primeiro número");
+            int sub1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            int sub2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da soma é: {sub1 - sub2}");
+          
+        }
+
+        static void Multiplicar()
+        {
+            Console.WriteLine("Vocês escolheu multiplicar \n");
+            Console.WriteLine("Digite o primeiro número");
+            int mult1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            int mult2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da soma é: {mult1 * mult2}");
+          }
+
+        static void Dividir()
+        {
+            Console.WriteLine("Vocês escolheu dividir");
+            Console.WriteLine("Digite o primeiro número");
+            int div1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            int div2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da soma é: {div1 * div2}");
+
+        }
+
+        static void Potencia()
+        {
+            Console.WriteLine("Digite a base da pontência");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o expoente pontência");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int potencia = (int)Math.Pow(number1, number2);
+
+            Console.WriteLine("O RESULTADO É: " + potencia);
+        }
+
+        static void Raiz()
+        {
+            Console.WriteLine("Digite o que será elevado a raiz");
+            int number1 = int.Parse(Console.ReadLine());
+            double raiz = Math.Sqrt(number1);
+
+            Console.WriteLine("O RESULTADO É: " + raiz);
+
+        }
+
+        static void Sair()
+        {
+            Console.WriteLine("Você escolheu sair. O programa será encerrado.");
+             
+
+        }
+
+            
+       
+    }
+}
---
+++ Curso 4 projetos/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace Curso_4_projetos
+{
+    internal class Program
+    {
+        enum EMenu { Somar = 1, Subtrair, Multiplicar, Dividir, Potencia, Raiz, Sair}
+
+        static void Main(string[] args)
+        {
+            bool escolheuSair = false;
+            while(!escolheuSair) 
+            {
+                Console.WriteLine("Digite uma opção");
+                Console.WriteLine("1 - Somar\n2 - Subtrair\n3 - Multiplicar\n4 - Divdir\n5 - Potencia\n6 - Raiz\n7 - Sair");
+
+                EMenu opcaoEscohida = (EMenu)int.Parse(Console.ReadLine());
+                Console.WriteLine(opcaoEscohida);
+
+                switch (opcaoEscohida)
+                {
+                    case EMenu.Somar:
+                        Soma();
+                        break;
+                    case EMenu.Subtrair:
+                        Subtrair();
+                        break;
+                    case EMenu.Multiplicar:
+                        Multiplicar();
+                        break;
+                    case EMenu.Dividir:
+                        Divisao();
+                        break;
+                    case EMenu.Potencia:
+                        Potencia();
+                        break;
+                    case EMenu.Raiz:
+                        Raiz();
+                        break;
+                    case EMenu.Sair:
+                        Console.WriteLine("Você escolheu sair. Boa sorte, até a próxima");
+                        escolheuSair = true;
+                        break;
+                }
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+            static void Soma()
+            {
+                Console.WriteLine("Digite o primeiro número");
+                int number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o segunda número");
+                int number2 = int.Parse(Console.ReadLine());
+                int soma = number1 + number2;
+
+                Console.WriteLine("O RESULTADO É: " + soma);
+            }
+
+            static void Subtrair()
+            {
+                Console.WriteLine("Digite o primeiro número");
+                int number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o segunda número");
+                int number2 = int.Parse(Console.ReadLine());
+                int subtracao = number1 - number2;
+
+                Console.WriteLine("O RESULTADO É: " + subtracao);
+            }
+
+            static void Multiplicar()
+            {
+                Console.WriteLine("Digite o primeiro número");
+                int number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o segunda número");
+                int number2 = int.Parse(Console.ReadLine());
+                int multiplicacao = number1 * number2;
+
+                Console.WriteLine("O RESULTADO É: " + multiplicacao);
+            }
+
+            static void Divisao()
+            {
+                Console.WriteLine("Digite o primeiro número");
+                int number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o segunda número");
+                int number2 = int.Parse(Console.ReadLine());
+                float divisao = (float)number1 / (float)number2;
+
+                Console.WriteLine("O RESULTADO É: " + divisao);
+            }
+
+            static void Potencia()
+            {
+                Console.WriteLine("Digite a base da pontência");
+                int number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o expoente pontência");
+                int number2 = int.Parse(Console.ReadLine());
+
+                int potencia = (int)Math.Pow(number1, number2);
+
+                Console.WriteLine("O RESULTADO É: " + potencia);
+            }
+
+            static void Raiz()
+            {
+                Console.WriteLine("Digite o que será elevado a raiz");
+                int number1 = int.Parse(Console.ReadLine());
+                double raiz = Math.Sqrt(number1);
+
+                Console.WriteLine("O RESULTADO É: " + raiz);
+            }
+        }
+    }
+}
